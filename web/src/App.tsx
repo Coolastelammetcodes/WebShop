3e2c3b87827f069d51f841462d528a8d0d3a66ac
isacgustavsson <117119420+isacgustavsson@users.noreply.github.com>
2026-09-07 18:00:54 +0200

    added get-all / post route for mugs in api

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -12,9 +12,11 @@ export default function App() {
     <>
       <Header></Header>
       <button onClick={() => query.refetch()}>test</button>
-      <p>
-        {query.data} - {query.dataUpdatedAt}
-      </p>
+      <div>
+        {query.data?.map((mug: any, index: any) => (
+          <p key={index}>{mug.name}</p>
+        ))}
+      </div>
     </>
   );
 }
