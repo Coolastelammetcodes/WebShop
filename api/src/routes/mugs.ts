4b4891476897f2d7efd266864541dc3774413444
shahadAlroomi <shahad.ro@icloud.com>
2026-09-08 11:10:17 +0200

    connect mugs endpoint to Prisma

--- api/src/routes/mugs.ts
+++ api/src/routes/mugs.ts
@@ -1,17 +1,20 @@
 import { Hono } from "hono";
+import { db } from "../data/db.ts";
 
-const mugs = new Hono()
+const mugs = new Hono();
 
 const mugArray: any[] = [];
 
 mugs.get("/", async (c) => {
-    return c.json(mugArray, 200)
-})
+  const res = await db.mugs.findMany();
+
+  return c.json(res, 200);
+});
 
 mugs.post("/", async (c) => {
-    const mug = await c.req.json()
-    mugArray.push(mug);
-    return c.json("thanks", 201);
-})
+  const mug = await c.req.json();
+  mugArray.push(mug);
+  return c.json("thanks", 201);
+});
 
 export default mugs;
