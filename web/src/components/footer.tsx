2d6747816c8c308977b289f7b6e5365cd5424491
shahadAlroomi <shahad.ro@icloud.com>
2026-09-10 23:38:11 +0200

    update style

--- web/src/components/footer.tsx
+++ web/src/components/footer.tsx
@@ -1,6 +1,8 @@
 import { Link } from "react-router";
 
 import Box from "@mui/material/Box";
+import Button from "@mui/material/Button";
+import Divider from "@mui/material/Divider";
 import Paper from "@mui/material/Paper";
 import Typography from "@mui/material/Typography";
 
@@ -10,39 +12,73 @@ export default function Footer() {
       component="footer"
       elevation={3}
       sx={{
-        padding: 3,
-        marginTop: 4,
-        textAlign: "center",
-        backgroundColor: "hsl(19, 34%, 25%)",
-        color: "white",
+        mt: 4,
+        px: 2,
+        py: 2,
+        backgroundColor: "#f0f0f0",
+        color: "black",
       }}
     >
-      <Typography variant="body2">@2026 Webshop</Typography>
-
       <Box
-        component="nav"
         sx={{
           display: "flex",
-          justifyContent: "center",
-          gap: 3,
-          margin: 2,
-          color: "white",
+          alignItems: "center",
+          paddingBottom: 1,
         }}
       >
-        <Link to="/" style={{ color: "white" }}>
-          Home
-        </Link>
+        <Typography
+          variant="body2"
+          sx={{
+            fontWeight: 600,
+            letterSpacing: "0.5px",
+            fontFamily: "Georgia, serif",
+          }}
+        >
+          © 2026 WebShop
+        </Typography>
 
-        <Link to="/shopping-cart" style={{ color: "white" }}>
-          Shopping Cart
-        </Link>
+        <Box
+          component="nav"
+          sx={{
+            display: "flex",
+            gap: 3,
+            marginLeft: "auto",
+          }}
+        >
+          <Link to="/" style={{ color: "#333" }}>
+            Home
+          </Link>
 
-        <Link to="/admin" style={{ color: "white" }}>
-          Admin
-        </Link>
+          <Link to="/shopping-cart" style={{ color: "#333" }}>
+            Shopping Cart
+          </Link>
+
+          <Link to="/admin" style={{ color: "#333" }}>
+            Admin
+          </Link>
+        </Box>
       </Box>
 
-      <Typography variant="body2">Contact us</Typography>
+      <Divider sx={{ backgroundColor: "#555", mb: 3 }} />
+      <Box
+        sx={{
+          display: "flex",
+          justifyContent: "space-between",
+          gap: 4,
+        }}
+      >
+        <Box>
+          <Typography variant="body2">Stay Updated</Typography>
+
+          <Box sx={{ display: "flex", gap: 1, mt: 1, mr: 2 }}>
+            <input placeholder="Your Email" />
+
+            <Button variant="contained">Subscribe</Button>
+          </Box>
+        </Box>
+
+        <Typography variant="body2">Contact us</Typography>
+      </Box>
     </Paper>
   );
 }
