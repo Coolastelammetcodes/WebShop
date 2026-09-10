ef7c8e774414564edcc5d54463d41fca1662ed93
isacgustavsson <117119420+isacgustavsson@users.noreply.github.com>
2026-09-10 15:56:46 +0200

    moved breadcrumb component to dashboard

--- web/src/routes/dashboard/dashboard.tsx
+++ web/src/routes/dashboard/dashboard.tsx
@@ -1,9 +1,11 @@
+import Breadcrumbs from "../../components/breadcrumbs";
 import { AddProductForm } from "./addProductForm";
 import { DisplayProducts } from "./displayProducts";
 
 export function Dashboard() {
   return (
     <>
+      <Breadcrumbs currentPage="Admin" />
       <h1>Dashboard</h1>
       <AddProductForm />
       <DisplayProducts />
