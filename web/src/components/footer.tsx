01b7f0ed63f00519c3c6d3ab55f465b95f963845
shahadAlroomi <shahad.ro@icloud.com>
2026-09-10 13:17:31 +0200

    Style footer and add links

--- web/src/components/footer.tsx
+++ web/src/components/footer.tsx
@@ -1,16 +1,33 @@
 import { Link } from "react-router";
 
-export default function footer() {
+import Box from "@mui/material/Box";
+
+export default function Footer() {
   return (
-    <footer>
+    <Box
+      component="footer"
+      sx={{
+        padding: 3,
+        textAlign: "center",
+        marginTop: 4,
+      }}
+    >
       <p>@2026 Webshop</p>
-      <nav>
+
+      <Box
+        component="nav"
+        sx={{
+          display: "flex",
+          justifyContent: "center",
+          gap: 3,
+        }}
+      >
         <Link to="/">Home</Link>
         <Link to="/shopping-cart ">Shopping Cart</Link>
         <Link to="/Admin">Admin</Link>
-      </nav>
+      </Box>
 
       <p>Contact us</p>
-    </footer>
+    </Box>
   );
 }
