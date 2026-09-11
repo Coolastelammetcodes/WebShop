b32168a48d88b14496996812b44833f8470f1d2e
shahadAlroomi <shahad.ro@icloud.com>
2026-09-11 11:45:00 +0200

    update shopping cart

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -3,14 +3,14 @@ import { BrowserRouter, Route, Routes } from "react-router";
 import Footer from "./components/footer";
 import Header from "./components/header";
 import { Dashboard } from "./routes/dashboard/dashboard";
+import type { Mug } from "./routes/dashboard/mugSchema";
 import { Home } from "./routes/home";
 import { ShoppingCart } from "./routes/shopping-cart";
 
 export default function App() {
-  const [cart, setCart] = useState<any[]>([]);
-  function AddToCart(mug: any) {
-    console.log("Add to cart", mug);
-    // setCart([...cart, mug]);
+  const [cart, setCart] = useState<Mug[]>([]);
+  function AddToCart(mug: Mug) {
+    setCart([...cart, mug]);
   }
   return (
     <>
@@ -20,7 +20,10 @@ export default function App() {
         <Routes>
           <Route path="/" element={<Home addToCart={AddToCart} />}></Route>
           <Route path="/dashboard" element={<Dashboard />}></Route>
-          <Route path="/shopping-cart" element={<ShoppingCart />}></Route>
+          <Route
+            path="/shopping-cart"
+            element={<ShoppingCart cart={cart} />}
+          ></Route>
         </Routes>
         <Footer />
       </BrowserRouter>
