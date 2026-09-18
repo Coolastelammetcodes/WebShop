d245193d6d27bbd9f1532a99e6f7716c5b4eddb5
shahadAlroomi <shahad.ro@icloud.com>
2026-09-18 14:42:25 +0200

    Add product card navigation

--- web/src/components/product-card.tsx
+++ web/src/components/product-card.tsx
@@ -1,11 +1,12 @@
+import { CardActionArea } from "@mui/material";
 import Button from "@mui/material/Button";
 import Card from "@mui/material/Card";
 import CardContent from "@mui/material/CardContent";
 import CardMedia from "@mui/material/CardMedia";
 import Typography from "@mui/material/Typography";
-import type { Mug } from "../types/mug";
+
 import { Link } from "react-router";
-import { CardActionArea } from "@mui/material";
+import type { Mug } from "../types/mug";
 
 type ProductCardProps = {
   mug: Mug;
@@ -17,82 +18,82 @@ export default function ProductCard({ mug, addToCart }: ProductCardProps) {
     <Card
       sx={{
         width: 280,
-        height: 580,
+        height: 520,
         borderRadius: "8px",
         boxShadow: 2,
         display: "flex",
         flexDirection: "column",
+        cursor: "pointer",
       }}
     >
-      <CardActionArea component={Link} to={`/mugs/${mug.id}`}>
-      <CardMedia
-        component="img"
-        image={mug.filepath}
-        alt={`bild på ${mug.name}`}
-        sx={{
-          width: "100%",
-          height: 300,
-          objectFit: "cover",
-          flexShrink: 0,
-        }}
-      />
+      <CardActionArea component={Link} to={`/product/${mug.id}`}>
+        <CardMedia
+          component="img"
+          image={mug.filepath}
+          alt={`bild på ${mug.name}`}
+          sx={{
+            width: "100%",
+            height: 300,
+            objectFit: "cover",
+            flexShrink: 0,
+          }}
+        />
 
-      <CardContent
+        <CardContent
+          sx={{
+            height: 220,
+            boxSizing: "border-box",
+            display: "flex",
+            flexDirection: "column",
+          }}
+        >
+          <Typography
+            gutterBottom
+            variant="h5"
+            component="div"
+            sx={{
+              fontFamily: "Georgia, serif",
+              fontWeight: 500,
+            }}
+          >
+            {mug.name}
+          </Typography>
+
+          <Typography
+            variant="body2"
+            sx={{
+              color: "text.secondary",
+            }}
+          >
+            {mug.description}
+          </Typography>
+
+          <Typography
+            variant="body1"
+            sx={{
+              fontWeight: 500,
+              mt: 1,
+            }}
+          >
+            {mug.price} kr
+          </Typography>
+        </CardContent>
+      </CardActionArea>
+      <Button
+        variant="contained"
+        onClick={() => addToCart(mug)}
         sx={{
-          height: 220,
-          boxSizing: "border-box",
-          display: "flex",
-          flexDirection: "column",
+          mt: "auto",
+          alignSelf: "flex-start",
+          backgroundColor: "#7A5236",
+          borderRadius: "20px",
+          "&:hover": {
+            backgroundColor: "#5F3E29",
+          },
         }}
       >
-        <Typography
-          gutterBottom
-          variant="h5"
-          component="div"
-          sx={{
-            fontFamily: "Georgia, serif",
-            fontWeight: 500,
-          }}
-        >
-          {mug.name}
-        </Typography>
-
-        <Typography
-          variant="body2"
-          sx={{
-            color: "text.secondary",
-          }}
-        >
-          {mug.description}
-        </Typography>
-
-        <Typography
-          variant="body1"
-          sx={{
-            fontWeight: 500,
-            mt: 1,
-          }}
-        >
-          {mug.price} kr
-        </Typography>
-
-      </CardContent>
-      </CardActionArea>
-        <Button
-          variant="contained"
-          onClick={() => addToCart(mug)}
-          sx={{
-            mt: "auto",
-            alignSelf: "flex-start",
-            backgroundColor: "#7A5236",
-            borderRadius: "20px",
-            "&:hover": {
-              backgroundColor: "#5F3E29",
-            },
-          }}
-        >
-          ADD TO CART
-        </Button>
+        ADD TO CART
+      </Button>
     </Card>
   );
 }
